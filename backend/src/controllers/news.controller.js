43f393b8a981c1fff4533b5d9dbc3c4f3d8b0e3d
hetvip215 <hetvipatel2934@gamil.com>
2026-06-19 12:43:14 +0530

    Add Redis caching for NewsAPI search results

--- backend/src/controllers/news.controller.js
+++ backend/src/controllers/news.controller.js
@@ -1,18 +1,47 @@
 import axios from 'axios';
 import {ApiError} from "../utils/ApiError.js"
 import { ApiResponse } from '../utils/ApiResponse.js'; // Adjust path based on your structure
+import redisClient from "../db/redis.js";
 
 const NEWS_API_URL = 'https://newsapi.org/v2/everything'; // or top-headlines
 //const API_KEY = process.env.NEWS_API_KEY;
 //console.log("api key",API_KEY);
 //"8a4290dbbe4b4eada47acca4b66dd41d"
-
+const CACHE_TTL = 60 * 5;
 export const getNews = async (req, res) => {
     const API_KEY = process.env.NEWS_API_KEY;
     //console.log("api key",API_KEY);
   const { q } = req.query;
 
+  const searchQuery = q || "technology";
+  const cacheKey = `news:${searchQuery.toLowerCase()}`;
+
   try {
+    
+    try {
+
+      const cachedNews = await redisClient.get(cacheKey);
+  
+      if (cachedNews) {
+          console.log(` CACHE HIT: ${searchQuery}`);
+  
+          return res.status(200).json(
+              new ApiResponse(
+                  200,
+                  JSON.parse(cachedNews),
+                  "News fetched successfully (Redis Cache)"
+              )
+          );
+      }
+  
+      console.log(` CACHE MISS: ${searchQuery}`);
+  
+    } catch (err) {
+    
+        console.warn("⚠️ Redis unavailable, fetching from NewsAPI...");
+    
+    }
+
     const response = await axios.get(NEWS_API_URL, {
       params: {
         q: q || 'technology', // default keyword
@@ -22,6 +51,19 @@ export const getNews = async (req, res) => {
       }
     });
     
+    try {
+
+      await redisClient.setEx(
+          cacheKey,
+          CACHE_TTL,
+          JSON.stringify(response.data.articles)
+      );
+  
+    } catch (err) {
+    
+        console.warn(" Failed to cache data in Redis");
+    
+    }
 
     return res.status(200).json(
       new ApiResponse(200, response.data.articles, "News fetched successfully")
--- backend/src/index.js
+++ backend/src/index.js
@@ -1,5 +1,6 @@
 import dotenv from "dotenv"
 import connectDB from "./db/index.js";
+import redisClient from "./db/redis.js";
 import {app} from './app.js'
 
 dotenv.config({
@@ -7,11 +8,16 @@ dotenv.config({
 })
 
 connectDB()
-.then(()=>{
-    app.listen(process.env.PORT || 8000, ()=>{
+.then(async () => {
+
+    await redisClient.connect();
+    console.log("Redis Connected");
+
+    app.listen(process.env.PORT || 8000, () => {
         console.log(`Server is running at ${process.env.PORT}`);
-    })
+    });
+
 })
-.catch((err)=>{
-    console.log("MONGODB connection failed !!",err);
-})
+.catch((err) => {
+    console.log("Connection failed", err);
+});
